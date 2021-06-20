791af7f129c17db08c9d884c09758e5418d36784
石井将司 <optacos@gmail.com>
2021-06-20 21:42:06 +0900

    UPDATE: multi handler

--- src/main.go
+++ src/main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
+type HelloHandler struct{}
+
+func (h *HelloHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
+}
+
+type WorldHandler struct{}
+
+func (h *WorldHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "World!")
+}
+
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
 func main() {
+	handler := MyHandler{}
+	hello := HelloHandler{}
+	world := WorldHandler{}
+
 	server := http.Server{
 		Addr:		"127.0.0.1:8000",
-		Handler:	nil,
 	}
+
+	http.Handle("/", &handler)
+	http.Handle("/hello", &hello)
+	http.Handle("/world", &world)
+
 	server.ListenAndServe()
 
 	// https用 設定
