68453a9d6043943bec2f2185c323a313481fdbb2
石井将司 <optacos@gmail.com>
2021-06-22 22:48:11 +0900

    response json

--- src/main.go
+++ src/main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"encoding/json"
 	"net/http"
 )
+
+type Post struct {
+	User	string
+	Threads	[]string
+}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello!")
@@ -32,6 +38,34 @@ func process(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func writeExample(w http.ResponseWriter, r *http.Request) {
+	str := `<html>
+<head><title>Go Web Programming</title></head>
+<body><h1>Hello World</h1></body>
+</html>`
+	w.Write([]byte(str))
+}
+
+func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(501)
+	fmt.Fprintf(w, "そのようなサービスはありません。他をあたってください。")
+}
+
+func headerExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Location", "http://google.com")
+	w.WriteHeader(302)
+}
+
+func jsonExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	post := &Post{
+		User:		"Sau Sheong",
+		Threads:	[]string{"1番目", "2番目", "3番目"},
+	}
+	json, _ := json.Marshal(post)
+	w.Write(json)
+}
+
 func main() {
 	server := http.Server{
 		Addr:		"127.0.0.1:8000",
@@ -41,5 +75,9 @@ func main() {
 	http.HandleFunc("/headers", headers)
 	http.HandleFunc("/body", body)
 	http.HandleFunc("/process", process)
+	http.HandleFunc("/write", writeExample)
+	http.HandleFunc("/writeheader", writeHeaderExample)
+	http.HandleFunc("/redirect", headerExample)
+	http.HandleFunc("/json", jsonExample)
 	server.ListenAndServe()
 }
