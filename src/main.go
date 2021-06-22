28b5f008aadc88b59fc03dd6caeeac8aa85ceb49
石井将司 <optacos@gmail.com>
2021-06-22 23:29:47 +0900

    use cookie

--- src/main.go
+++ src/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"encoding/json"
+	"encoding/base64"
 	"net/http"
+	"time"
 )
 
 type Post struct {
@@ -66,6 +68,60 @@ func jsonExample(w http.ResponseWriter, r *http.Request) {
 	w.Write(json)
 }
 
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	c1 := http.Cookie{
+		Name:		"first_cookie",
+		Value:		"Go Web Programing",
+		HttpOnly:	true,
+	}
+	c2 := http.Cookie{
+		Name:		"second_cookie",
+		Value:		"Manning Publications Co",
+		HttpOnly:	true,
+	}
+	// w.Header().Set("Set-Cookie", c1.String())
+	// w.Header().Add("Set-Cookie", c2.String())
+	http.SetCookie(w, &c1)
+	http.SetCookie(w, &c2)
+}
+
+func getCookie(w http.ResponseWriter, r *http.Request) {
+	cl, err := r.Cookie("first_cookie")
+	if err != nil {
+		fmt.Fprintln(w, "Cannot get the first cookie")
+	}
+	cs := r.Cookies()
+	fmt.Fprintln(w, cl)
+	fmt.Fprintln(w, cs)
+}
+
+func setMessage(w http.ResponseWriter, r *http.Request) {
+	msg := []byte("Hello World!")
+	c := http.Cookie{
+		Name:	"flash",
+		Value:	base64.URLEncoding.EncodeToString(msg),	// ヘッダ内ではクッキの値をURLエンコードする必要がある
+	}
+	http.SetCookie(w, &c)
+}
+
+func showMessage(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("flash")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			fmt.Fprintf(w, "メッセージがありません")
+		}
+	} else {
+		rc := http.Cookie{
+			Name:		"flash",
+			MaxAge:		-1,
+			Expires:	time.Unix(1, 0),
+		}
+		http.SetCookie(w, &rc)
+		val, _ := base64.URLEncoding.DecodeString(c.Value)
+		fmt.Fprintln(w, string(val))
+	}
+}
+
 func main() {
 	server := http.Server{
 		Addr:		"127.0.0.1:8000",
@@ -79,5 +135,9 @@ func main() {
 	http.HandleFunc("/writeheader", writeHeaderExample)
 	http.HandleFunc("/redirect", headerExample)
 	http.HandleFunc("/json", jsonExample)
+	http.HandleFunc("/set_cookie", setCookie)
+	http.HandleFunc("/get_cookie", getCookie)
+	http.HandleFunc("/set_message", setMessage)
+	http.HandleFunc("/show_message", showMessage)
 	server.ListenAndServe()
 }
